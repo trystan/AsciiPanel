c42dbb0cc2ac0c1dc97afe8ad82632860a4a5558
zn80 <zn80@hacari.com>
2015-02-20 19:36:04 +0100

    Added new Fonts
    Added new Font definitions

--- src/asciiPanel/AsciiFont.java
+++ src/asciiPanel/AsciiFont.java
@@ -10,11 +10,12 @@ package asciiPanel;
 public enum AsciiFont {
 
 	CP437_8x8("cp437_8x8.png", 8, 8),
-	CP437_10x10("cp437_10x10.png",10,10),
+	CP437_10x10("cp437_10x10.png", 10, 10),
 	CP437_12x12("cp437_12x12.png", 12, 12),
 	CP437_16x16("cp437_16x16.png", 16, 16),
 	CP437_9x16("cp437_9x16.png", 9, 16),
 	DRAKE_10x10("drake_10x10.png", 10, 10),
+	TAFFER_10x10("taffer_10x10.png", 10, 10),
 	QBICFEET_10x10("qbicfeet_10x10.png", 10, 10),
 	TALRYTH_15_15("talryth_square_15x15.png", 15, 15);
 	
